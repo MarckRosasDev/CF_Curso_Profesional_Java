b686f71f36b55800719582a75dbeb4b7872d22ef
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-11-23 19:58:51 -0600

    Actualización archivo Variables.java

--- src/main/java/Variables.java
+++ src/main/java/Variables.java
@@ -15,7 +15,16 @@ public class Variables {
         System.out.println(letra);
         System.out.println(numero);
 
+        boolean valor1 = true;
+        boolean valor2 = false;
 
+        byte diasMes = 31; //Aproximadamente
+        short diasLustro = (12 * 31) * 5;
+        int velocidadLuz = 299792458;
+        long añoLuz = velocidadLuz * 365;
+
+        float pi = 3.1415926535f;
+        double e = 2.718281828459045235360;
         
     }
 }
