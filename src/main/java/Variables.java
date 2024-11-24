8d02f2da4b039f3eb30e91d788ce3777cb64e4c9
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-11-23 19:41:53 -0600

    Adición del archivo Variables.java

---
+++ src/main/java/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args) {
+        /*
+        * 4 tipos de datos
+        * Enteros -- byte, short, int, long
+        * Decimales -- float, double
+        * Caracteres -- char
+        * Booleanos -- booleano
+        * */
+
+        int version = 9;
+        float versionEspecifica = 9.8f;
+        char letra = 'a';
+        char numero = 97;
+        System.out.println(letra);
+        System.out.println(numero);
+
+
+        
+    }
+}
