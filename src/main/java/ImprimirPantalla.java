e92fa66cf05c6c8273e0d7575527b9ca5c060e33
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-11-24 09:29:35 -0600

    Adición del archivo ImprimirPantalla.java

---
+++ src/main/java/ImprimirPantalla.java
@@ -0,0 +1,20 @@
+public class ImprimirPantalla {
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+        System.out.println("Mi nombre es Codi");
+
+        System.out.print("Hola Mundo");
+        System.out.print("Mi nombre es Codi");
+
+        System.out.print("Hola,\nagregamos un salto");
+        System.out.print("\tagregamos un tab");
+        System.out.println("");
+
+        System.out.println("Mensaje con \\ diagonal inversa");
+        System.out.println("Mensaje con \" comillas dobles");
+        System.out.println("Mensaje con \' comillas simples");
+
+    }
+
+}
