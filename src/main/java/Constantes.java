400b42cb138085f169f2bda632003996366e2e26
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-11-26 21:19:43 -0600

    Adición del archivo "Constantes.java"

---
+++ src/main/java/Constantes.java
@@ -0,0 +1,13 @@
+public class Constantes {
+    public static void main(String[] args) {
+
+        final float pi = 3.1416f, e = 8.9f;
+
+        System.out.println(pi);
+        System.out.println(e);
+
+        // pi=3.1415f; Indica error
+
+    }
+
+}
