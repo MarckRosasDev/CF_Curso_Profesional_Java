25e22b8454fc20e175746364dd8eab3e3be5d34f
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-11-26 18:48:13 -0600

    Adición del archivo "MúltiplesVariables.java"

---
+++ src/main/java/MultiplesVariables.java
@@ -0,0 +1,13 @@
+public class MultiplesVariables {
+    public static void main(String[] args) {
+        int variable1 = 1, variable2 = 2, variable3 = 3;
+        float pi = 3.1415f, e = 0.1010f;
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(pi);
+        System.out.println(e);
+
+    }
+}
