aca3f79831bb8dcffb7c155bb82f20bce1b1e0c0
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-11-29 20:07:25 -0600

    Adición del archivo OperadoresRelacionales.java

---
+++ src/main/java/OperadoresRelacionales.java
@@ -0,0 +1,26 @@
+public class OperadoresRelacionales {
+    public static void main(String[] args) {
+
+        int variable1 = 300, variable2 = 200;
+        boolean resultado;
+
+        resultado = variable1 > variable2;
+        System.out.println(resultado);
+
+        resultado = variable1 < variable2;
+        System.out.println(resultado);
+
+        resultado = variable1 >= variable2;
+        System.out.println(resultado);
+
+        resultado = variable1 <= variable2;
+        System.out.println(resultado);
+
+        resultado = variable1 == variable2;
+        System.out.println(resultado);
+
+        resultado = variable1 != variable2;
+        System.out.println(resultado);
+
+    }
+}
