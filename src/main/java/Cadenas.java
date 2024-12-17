d7fb234033a672ea3682f4860f0d593c3078a6d5
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-12-17 00:24:12 -0600

    Adición del archivo "Cadenas.java"

---
+++ src/main/java/Cadenas.java
@@ -0,0 +1,10 @@
+public class Cadenas {
+    public static void main(String[] args) {
+        String mensaje = "Hola, soy un String";
+        String mensaje2 = new String("Texto");
+
+        System.out.println(mensaje);
+        System.out.println(mensaje2);
+    }
+
+}
